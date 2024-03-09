bbeee5115ae9e33a2599c143bc88d9e59d6e817d
Blue0Tech <pruthvishrikaanth@gmail.com>
2024-03-09 10:17:25 +0000

    Basic mailer setup created

--- server.js
+++ server.js
@@ -4,3 +4,11 @@ const server = require("http").Server(app);
 app.use(express.json())
 
 server.listen(process.env.PORT || 3030);
+
+let nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    secure: true
+});
